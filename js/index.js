e8bff8f24bf3afdd3c7491ce3374c7370072ab63
tranenn <aamandatran@gmail.com>
2023-09-30 16:21:08 +0200

    Declared variable for header, main and footer

---
+++ js/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Declaring all the variables that will be used repeatedly
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
