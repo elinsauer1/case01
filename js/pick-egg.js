efa892f865f35854d70e2e71c317b285754b8d27
tranenn <aamandatran@gmail.com>
2023-09-30 18:40:40 +0200

    Added radio buttons for consistency and size

--- js/pick-egg.js
+++ js/pick-egg.js
@@ -1,5 +1,63 @@
 "use strict";
 
+pickYourEgg()
 function pickYourEgg() {
+    main.innerHTML = `
+        <h1>PICK YOUR EGG</h1>
 
+        <h3>CONSISTENCY</h3>
+        <section>
+            <div class="radio-button-group">
+                <div class="input-container">
+                    <input id="liquid" type="radio" name="radio-consistency">
+                    <div class="radio-button">
+                        <div id="liquid-curve" class="curve-png"></div>
+                    </div>
+                    <label for="liquid">Liquid</label>
+                </div>
+                <div class="input-container">
+                    <input id="soft" type="radio" name="radio-consistency">
+                    <div class="radio-button">
+                        <div id="soft-curve" class="curve-png"></div>
+                    </div>
+                    <label for="soft">Soft</label>
+                </div>
+                <div class="input-container">
+                    <input id="hard" type="radio" name="radio-consistency">
+                    <div class="radio-button">
+                        <div id="hard-curve" class="curve-png"></div>
+                    </div>
+                    <label for="hard">Hard</label>
+                </div>
+            </div>
+        </section>
+        
+        <h3>SIZE</h3>
+        <section>
+            <h3></h3>
+            <div class="radio-button-group">
+                <div class="input-container">
+                    <input id="small" type="radio" name="radio-size">
+                    <div class="radio-button">
+                        <div>S</div>
+                    </div>
+                    <label for="small">Small</label>
+                </div>
+                <div class="input-container">
+                    <input id="medium" type="radio" name="radio-size">
+                    <div class="radio-button">
+                        <div>M</div>
+                    </div>
+                    <label for="medium">Medium</label>
+                </div>
+                <div class="input-container">
+                    <input id="large" type="radio" name="radio-size">
+                    <div class="radio-button">
+                        <div>L</div>
+                    </div>
+                    <label for="large">Large</label>
+                </div>
+            </div>
+        </section>
+    `;
 } 
