bf1071da31a32974fe99f3a0e324f14966b475cb
elinsauer1 <elinsauer@hotmail.com>
2023-10-02 12:28:50 +0200

    added changes

--- js/egg-timer.js
+++ js/egg-timer.js
@@ -9,9 +9,11 @@ function EggTimerView() {
     <div class="color-container">
         <div class="color-cover"></div>
     </div>
-    <div id="egg">
-        <div class="time-container">
-            <p class="time">0:00</p>
+    <div id="egg-container">
+        <div class="egg">
+            <div class="time-container">
+                <p class="time">0:00</p>
+            </div>
         </div>
     </div>
     <div id="footer-container">
@@ -25,12 +27,47 @@ function EggTimerView() {
     //Skapar transition
     start_button.addEventListener("click", () => {
 
+        document.querySelector(".start-button").textContent = "RESET";
+
+        const minutes = 0;
+        const seconds = 10;
+
+        countdown(minutes, seconds);
+
+        const total_time = (minutes * 60) + seconds + "s";
+        console.log(total_time);
+
         const color_cover = document.querySelector(".color-cover");
-        color_cover.style.transition = 'width 4s linear';
+        color_cover.style.transition = 'width ' + total_time + ' linear';;
         color_cover.style.width = '100%';
     });
 
-
-
 }
+
+var timeoutHandle;
+function countdown(minutes, seconds) {
+    function tick() {
+
+        const counter = document.querySelector(".time");
+        counter.textContent =
+            minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        seconds--;
+        if (seconds >= 0) {
+            timeoutHandle = setTimeout(tick, 1000);
+
+        } else {
+            if (minutes >= 1) {
+                setTimeout(function () {
+                    countdown(minutes - 1, 59);
+                }, 1000);
+            } else {
+                if (seconds === -1 && minutes === 0) {
+                    console.log("hej");
+                }
+            }
+        }
+    }
+    tick();
+}
+
 
